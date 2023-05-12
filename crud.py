9cd68bfdd679b3e725d616147d1ca5ff1fee6fe5
Vittz_jor <orehovvv2007@gmail.com>
2023-05-12 14:01:16 +0300

    Initial commit

---
+++ crud.py
@@ -0,0 +1,53 @@
+import shutil
+from pathlib import Path
+
+from fastapi import UploadFile
+from sqlalchemy.orm import Session
+import models
+import schemas
+from uuid import uuid4
+import easyocr
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+async def save_file(upload_file: UploadFile) -> str:
+    destination = './user-files/' + str(uuid4()) + '.jpg'
+
+    try:
+        with open(destination, 'wb') as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+        return destination
