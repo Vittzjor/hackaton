320b62684e32c4772796266466449af362ff34c6
Vittzjor <orehovvv2007@gmail.com>
2024-06-15 22:41:07 +0300

    first commit3

--- main.py
+++ main.py
@@ -28,31 +28,34 @@ def get_db():
 
 # Загрузка данных из Excel
 def load_excel_data(excel_path):
+    # Загружаем данные из Excel файла
     df = pd.read_excel(excel_path)
-    return df
+    # Переименовываем столбцы для удобства
+    df.columns = [
+        "Код", "Категория", "Время регистрации", "Рабочая группа", 
+        "Краткое описание", "Описание", "Решение", "Аналитика 1", 
+        "Аналитика 2", "Аналитика 3"
+    ]
+    # Конвертируем DataFrame в список словарей
+    return df.to_dict(orient='records')
 
 # Создание эмбеддингов
 def create_embeddings(model, texts):
     return model.encode(texts)
 
-# Загрузка и подготовка данных
-excel_data = load_excel_data('./data.xlsx')
-questions = [item['question'] for item in excel_data]
+file_path = './data.xlsx'
+
+
+excel_data = load_excel_data('data.xlsx')
 
 # Создание модели эмбеддингов
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Создание эмбеддингов для Excel данных
-excel_embeddings = create_embeddings(embedding_model, questions)
 
 # Создание векторного индекса
-index = faiss.IndexFlatL2(excel_embeddings.shape[1])
-index.add(excel_embeddings)
 
 # Загрузка модели LLaMA
-model_name = 'open-llama'
-model = AutoModelForCausalLM.from_pretrained('model_name')
-tokenizer = AutoTokenizer.from_pretrained('model_name')
+
 
 # Функция генерации ответа
 def generate_response(prompt, model, tokenizer):
@@ -65,6 +68,14 @@ def generate_response(prompt, model, tokenizer):
 class ChatRequest(BaseModel):
     query: str
 
+@app.get("/data")
+def read_data():
+    return excel_data.to_dict(orient='records')
+
+@app.get("/questions")
+def get_questions():
+    questions = [item['Краткое описание'] for item in excel_data if 'Краткое описание' in item]
+    return questions
 # Маршрут для чата
 @app.post("/chat", response_model=schemas.Message)
 def chat(user: schemas.UserCreate, message: schemas.MessageCreate, db: Session = Depends(get_db)):
@@ -75,18 +86,15 @@ def chat(user: schemas.UserCreate, message: schemas.MessageCreate, db: Session =
     query_embedding = embedding_model.encode([user_query])
     
     # Поиск в индексе
-    D, I = index.search(query_embedding, k=1)
     
     # Получение наиболее релевантного ответа из Excel данных
-    relevant_document = excel_data[I[0][0]]['answer']
     
     # Генерация ответа с использованием модели LLaMA
-    prompt = f"User question: {user_query}\nRelevant document: {relevant_document}\nAnswer:"
-    response_text = generate_response(prompt, model, tokenizer)
+    
     
     # Создание сообщения в базе данных
     db_message = crud.create_message(db, message, user_id=db_user.id)
-    db_message.response = response_text
+    db_message.response = "фывапфыапф"
     db.commit()
     db.refresh(db_message)
     
